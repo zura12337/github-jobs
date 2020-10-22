509943fc98a72a6f3c6a0601b0ce5a6f8deca779
Zura Kurtanidze <zkurtanidze@pop-os.localdomain>
2020-10-22 14:58:49 +0400

    Updates

---
+++ precache-manifest.0b00125ae87ffc433e94ceb53f7aa190.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e332b4307e9e83910d471d6e74e4c6d9",
+    "url": "/github-jobs/index.html"
+  },
+  {
+    "revision": "eff8d12763551fa85d03",
+    "url": "/github-jobs/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "93003359a04639220f3b",
+    "url": "/github-jobs/static/css/main.62fadfb1.chunk.css"
+  },
+  {
+    "revision": "eff8d12763551fa85d03",
+    "url": "/github-jobs/static/js/2.69c87226.chunk.js"
+  },
+  {
+    "revision": "f406aedd96f61bd83ab19677f9afa536",
+    "url": "/github-jobs/static/js/2.69c87226.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "93003359a04639220f3b",
+    "url": "/github-jobs/static/js/main.129d2ccc.chunk.js"
+  },
+  {
+    "revision": "818ca7b73b62caac58fd",
+    "url": "/github-jobs/static/js/runtime-main.90d76a25.js"
+  },
+  {
+    "revision": "19459e6a27913ba8d46fadc65923308d",
+    "url": "/github-jobs/static/media/arrow-up-thick.19459e6a.svg"
+  },
+  {
+    "revision": "b87a440008e051faa11e44c41f385ff5",
+    "url": "/github-jobs/static/media/loading.b87a4400.svg"
+  }
+]);
